7492694673a30324fd85ffd5774701d3afaab7c5
JonasSteur <jonas@estuera.com>
2019-07-27 22:41:48 +0200

    Black formatting

--- coppermint/wsgi.py
+++ coppermint/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coppermint.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coppermint.settings")
 
 application = get_wsgi_application()
