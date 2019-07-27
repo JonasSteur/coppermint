0a4c07268d3f82ddd4d59998bf0d16550be7afa9
JonasSteur <jonas@estuera.com>
2019-07-27 23:54:12 +0200

    Add missing coinbase pro requirements + shell_plus

--- coppermint/settings.py
+++ coppermint/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # shell plus
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
